4b7db2b61f812b4f385e167429ed510e310697c2
Pedro Henrique Souza Tolentino <pedro_toletino12@hotmail.com>
2020-06-11 00:34:01 -0300

    Initial commit.

---
+++ src/main/java/com/financial/solutions/incomesynchronization/IncomeSynchronizationApplication.java
@@ -0,0 +1,13 @@
+package com.financial.solutions.incomesynchronization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncomeSynchronizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IncomeSynchronizationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/financial/solutions/incomesynchronization/IncomeSynchronizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.financial.solutions.incomesynchronization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncomeSynchronizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
